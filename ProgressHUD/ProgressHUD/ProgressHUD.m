e02dac7013ec6d22095cbb5460c4c4cba97f41aa
Greg Przybyla <greg@dropletapp.com>
2020-09-18 11:16:24 +0200

    Fix

--- ProgressHUD/ProgressHUD/ProgressHUD.m
+++ ProgressHUD/ProgressHUD/ProgressHUD.m
@@ -9,7 +9,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "OldProgressHUD.h"
+#import "ProgressHUD.h"
 
 @implementation ProgressHUD
 
